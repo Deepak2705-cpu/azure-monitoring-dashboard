c16a488e2206d8b81eacd47668d5c16e2ec4c9de
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-26 12:43:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,16 @@
+variable "location" {
+  description = "Azure region for monitoring resources"
+  type        = string
+  default     = "East US"
+}
 
+variable "teams_webhook_url" {
+  description = "Microsoft Teams incoming webhook URL for alert notifications"
+  type        = string
+  sensitive   = true
+}
+
+variable "vm_resource_id" {
+  description = "Resource ID of the VM to monitor"
+  type        = string
+}
