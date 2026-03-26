1a9cc19e7f411ab2c7eb96a74476f497cef5ae2f
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-26 12:43:28 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,21 @@
+output "log_analytics_workspace_id" {
+  description = "ID of the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.main.id
+}
 
+output "app_insights_instrumentation_key" {
+  description = "Application Insights instrumentation key"
+  value       = azurerm_application_insights.main.instrumentation_key
+  sensitive   = true
+}
+
+output "app_insights_connection_string" {
+  description = "Application Insights connection string"
+  value       = azurerm_application_insights.main.connection_string
+  sensitive   = true
+}
+
+output "action_group_id" {
+  description = "ID of the critical alerts action group"
+  value       = azurerm_monitor_action_group.critical.id
+}
